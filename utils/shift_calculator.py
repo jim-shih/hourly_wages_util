b2b70f39677e876d65734f44db3f9633ce5e7cc1
jimmmm-mm <m11001107@ntust.edu.tw>
2024-02-02 16:37:36 +0800

    feat: modify the calculation of salary (based on the law)

--- utils/shift_calculator.py
+++ utils/shift_calculator.py
@@ -1,9 +1,8 @@
-import json
 import dataclasses
-from datetime import datetime
+import json
 from collections import Counter
-from datetime import timedelta
-from typing import Dict, Any
+from datetime import datetime, timedelta
+from typing import Any, Dict
 
 
 @dataclasses.dataclass
@@ -22,6 +21,22 @@ class ShiftCalculator:
             data = json.load(file)
         return data
 
+    def calculate_overtime_pay(self, total_hours: timedelta) -> float:
+        total_hours = total_hours.total_seconds() / 3600
+        if total_hours <= 8:
+            return total_hours * self.hourly_rate
+        else:
+            overtime_hours = total_hours - 8
+            if overtime_hours <= 2:
+                return (8 * self.hourly_rate) + (overtime_hours *
+                                                 self.hourly_rate * 1.33)
+            else:
+                first_two_hours_overtime = 2 * self.hourly_rate * 1.33
+                remaining_overtime = (overtime_hours -
+                                      2) * self.hourly_rate * 1.66
+                return (8 * self.hourly_rate
+                        ) + first_two_hours_overtime + remaining_overtime
+
     @staticmethod
     def _calculate_shift_duration(start_time: str, end_time: str) -> timedelta:
         start = datetime.strptime(start_time, "%H:%M")
@@ -29,7 +44,8 @@ class ShiftCalculator:
         duration = end - start
         return duration
 
-    def calculate_total_working_time(self) -> timedelta:
+    def calculate_total_wage(self) -> (float, timedelta):
+        total_wage = 0
         total_working_time = timedelta(0)
         for shift, frequency in Counter(self.shifts.values()).items():
             if shift == "X" or shift not in self.shift_type:
@@ -37,27 +53,24 @@ class ShiftCalculator:
             shift_duration = self._calculate_shift_duration(
                 self.shift_type[shift]["start_time"],
                 self.shift_type[shift]["end_time"])
+            total_wage += self.calculate_overtime_pay(
+                shift_duration) * frequency
             total_working_time += shift_duration * frequency
-        return total_working_time
+        return total_wage, total_working_time
 
-    def calculate_total_working_hours(self) -> float:
-        total_working_time = self.calculate_total_working_time()
+    def calculate_total_working_hours(self) -> (float, float):
+        total_wage, total_working_time = self.calculate_total_wage()
         total_working_hours = total_working_time.total_seconds() / 3600
-        return total_working_hours
-
-    def calculate_total_salary(self) -> float:
-        total_working_hours = self.calculate_total_working_hours()
-        total_salary = total_working_hours * self.hourly_rate
-        return total_salary
+        return total_working_hours, total_wage
 
     def print_results(self) -> None:
-        total_working_hours = self.calculate_total_working_hours()
-        total_salary = self.calculate_total_salary()
+        total_working_hours, total_salary = self.calculate_total_working_hours(
+        )
         print(f"Total working hours: {total_working_hours}")
         print(f"Total salary: {total_salary}")
 
 
 if __name__ == "__main__":
     calculator = ShiftCalculator("./data/shift_type.json",
-                                 "./data/fab_shift.json", 190)
+                                 "./data/jan_shift.json", 190)
     calculator.print_results()
