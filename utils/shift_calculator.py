efa9dc41e4f763e05dd4d544b68d2036171b6223
jimmmm-mm <m11001107@ntust.edu.tw>
2024-02-03 15:37:21 +0800

    feat: enhance computational time, readability

--- utils/shift_calculator.py
+++ utils/shift_calculator.py
@@ -1,26 +1,52 @@
 import dataclasses
+import functools
 import json
-from collections import Counter
+import weakref
 from datetime import datetime, timedelta
+from pathlib import Path
 from typing import Any, Dict
+
+
+def memoized_method(*lru_args, **lru_kwargs):
+
+    def decorator(func):
+
+        @functools.wraps(func)
+        def wrapped_func(self, *args, **kwargs):
+            # We're storing the wrapped method inside the instance. If we had
+            # a strong reference to self the instance would never die.
+            self_weak = weakref.ref(self)
+
+            @functools.wraps(func)
+            @functools.lru_cache(*lru_args, **lru_kwargs)
+            def cached_method(*args, **kwargs):
+                return func(self_weak(), *args, **kwargs)
+
+            setattr(self, func.__name__, cached_method)
+            return cached_method(*args, **kwargs)
+
+        return wrapped_func
+
+    return decorator
 
 
 @dataclasses.dataclass
 class ShiftCalculator:
-    shift_type_file: str
-    shift_file: str
+    shift_type_file: Path
+    shift_file: Path
+    holiday_file: Path
     hourly_rate: float
 
     def __post_init__(self) -> None:
         self.shift_type = self._load_json_file(self.shift_type_file)
         self.shifts = self._load_json_file(self.shift_file)
+        self.holidays = self._load_json_file(self.holiday_file)
 
     @staticmethod
-    def _load_json_file(file_path: str) -> Dict[str, Any]:
-        with open(file_path, "r") as file:
-            data = json.load(file)
-        return data
+    def _load_json_file(file_path: Path) -> Dict[str, Any]:
+        return json.loads(file_path.read_text())
 
+    @memoized_method()
     def calculate_overtime_pay(self, total_hours: timedelta) -> float:
         total_hours = total_hours.total_seconds() / 3600
         if total_hours <= 8:
@@ -31,46 +57,49 @@ class ShiftCalculator:
                 return (8 * self.hourly_rate) + (overtime_hours *
                                                  self.hourly_rate * 1.33)
             else:
-                first_two_hours_overtime = 2 * self.hourly_rate * 1.33
-                remaining_overtime = (overtime_hours -
-                                      2) * self.hourly_rate * 1.66
-                return (8 * self.hourly_rate
-                        ) + first_two_hours_overtime + remaining_overtime
+                return (8 *
+                        self.hourly_rate) + (2 * self.hourly_rate * 1.33) + (
+                            (overtime_hours - 2) * self.hourly_rate * 1.66)
 
     @staticmethod
     def _calculate_shift_duration(start_time: str, end_time: str) -> timedelta:
         start = datetime.strptime(start_time, "%H:%M")
         end = datetime.strptime(end_time, "%H:%M")
-        duration = end - start
-        return duration
+        return end - start
 
-    def calculate_total_wage(self) -> (float, timedelta):
+    def calculate_total_working_hours_and_wage(self) -> (float, float):
         total_wage = 0
         total_working_time = timedelta(0)
-        for shift, frequency in Counter(self.shifts.values()).items():
-            if shift == "X" or shift not in self.shift_type:
+
+        for date, shift in self.shifts.items():
+            if shift == "X":
                 continue
-            shift_duration = self._calculate_shift_duration(
+
+            duration = self._calculate_shift_duration(
                 self.shift_type[shift]["start_time"],
                 self.shift_type[shift]["end_time"])
-            total_wage += self.calculate_overtime_pay(
-                shift_duration) * frequency
-            total_working_time += shift_duration * frequency
-        return total_wage, total_working_time
+            total_working_time += duration
 
-    def calculate_total_working_hours(self) -> (float, float):
-        total_wage, total_working_time = self.calculate_total_wage()
-        total_working_hours = total_working_time.total_seconds() / 3600
+            if date in self.holidays:
+                total_wage += duration.total_seconds(
+                ) / 3600 * self.hourly_rate * 2
+            else:
+                total_wage += self.calculate_overtime_pay(duration)
+
+        total_working_hours = round(total_working_time.total_seconds() / 3600,
+                                    2)
+        total_wage = round(total_wage, 2)
+
         return total_working_hours, total_wage
 
-    def print_results(self) -> None:
-        total_working_hours, total_salary = self.calculate_total_working_hours(
+    def __str__(self) -> str:
+        total_working_hours, total_salary = self.calculate_total_working_hours_and_wage(
         )
-        print(f"Total working hours: {total_working_hours}")
-        print(f"Total salary: {total_salary}")
+        return f"Total working hours: {total_working_hours}\nTotal salary: {total_salary}"
 
 
 if __name__ == "__main__":
-    calculator = ShiftCalculator("./data/shift_type.json",
-                                 "./data/jan_shift.json", 190)
-    calculator.print_results()
+    calculator = ShiftCalculator(Path("./data/shift_type.json"),
+                                 Path("./data/jan_shift.json"),
+                                 Path("./data/holiday.json"), 190)
+    print(calculator)
