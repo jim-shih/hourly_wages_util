6aee0930bca490ad3f333d4a8fbcf1a597c56bb2
jimmmm-mm <m11001107@ntust.edu.tw>
2024-02-15 16:17:25 +0800

    feat: enhance the readability and structure, calculation of wage.

--- utils/shift_calculator.py
+++ utils/shift_calculator.py
@@ -1,87 +1,70 @@
-import dataclasses
-import functools
 import json
-import weakref
+import numpy as np
 from datetime import datetime, timedelta
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
+from enum import Enum
 
 
-def memoized_method(*lru_args, **lru_kwargs):
-
-    def decorator(func):
-
-        @functools.wraps(func)
-        def wrapped_func(self, *args, **kwargs):
-            # We're storing the wrapped method inside the instance. If we had
-            # a strong reference to self the instance would never die.
-            self_weak = weakref.ref(self)
-
-            @functools.wraps(func)
-            @functools.lru_cache(*lru_args, **lru_kwargs)
-            def cached_method(*args, **kwargs):
-                return func(self_weak(), *args, **kwargs)
-
-            setattr(self, func.__name__, cached_method)
-            return cached_method(*args, **kwargs)
-
-        return wrapped_func
-
-    return decorator
+class ShiftType(Enum):
+    REST = "X"
 
 
-@dataclasses.dataclass
 class ShiftCalculator:
-    shift_type_file: Path
-    shift_file: Path
-    holiday_file: Path
-    hourly_rate: float
+    BASE_HOURS = 8
+    OVERTIME_HOURS_LIMIT = 2
+    OVERTIME_RATE_1 = 1.33
+    OVERTIME_RATE_2 = 1.66
+    HOLIDAY_RATE = 2
+    HOURLY_RATE = 190
 
-    def __post_init__(self) -> None:
+    def __init__(self, shift_type_file: Path, shift_file: Path, # noqa
+                 holiday_file: Path):  # noqa
+        self.shift_type_file = shift_type_file
+        self.shift_file = shift_file
+        self.holiday_file = holiday_file
         self.shift_type = self._load_json_file(self.shift_type_file)
         self.shifts = self._load_json_file(self.shift_file)
-        self.shifts = {
-            datetime.strptime(date, "%Y-%m-%d").date(): shift
-            for date, shift in self.shifts.items()
-        }
-
         self.holidays = self._load_json_file(self.holiday_file)
-        self.holidays = [
-            datetime.strptime(holiday, "%Y-%m-%d").date()
-            for holiday in self.holidays.keys()
-        ]
 
     @staticmethod
     def _load_json_file(file_path: Path) -> Dict[str, Any]:
+        """Load and return data from a JSON file."""
         return json.loads(file_path.read_text())
 
-    @memoized_method()
-    def calculate_overtime_pay(self, total_hours: timedelta) -> float:
-        total_hours = total_hours.total_seconds() / 3600
-        if total_hours <= 8:
-            return total_hours * self.hourly_rate
-        else:
-            overtime_hours = total_hours - 8
-            if overtime_hours <= 2:
-                return (8 * self.hourly_rate) + (overtime_hours *
-                                                 self.hourly_rate * 1.33)
-            else:
-                return (8 *
-                        self.hourly_rate) + (2 * self.hourly_rate * 1.33) + (
-                            (overtime_hours - 2) * self.hourly_rate * 1.66)
+    def calculate_overtime_pay(self, duty_hours: timedelta) -> float:
+        """Calculate the overtime pay based on the duty hours."""
+        duty_hours = duty_hours.total_seconds() / 3600
+        resting_hours = np.ceil(duty_hours / 4) * 0.5
+        working_hours = np.floor([duty_hours - resting_hours])
+        overtime_hours = np.floor(np.max([0, duty_hours - self.BASE_HOURS]))
+
+        if duty_hours <= self.BASE_HOURS:
+            return working_hours * self.HOURLY_RATE
+        elif overtime_hours <= self.OVERTIME_HOURS_LIMIT:
+            return (self.BASE_HOURS * self.HOURLY_RATE) + (
+                overtime_hours * self.HOURLY_RATE * self.OVERTIME_RATE_1)
+        else:  # overtime_hours > 2
+            return (self.BASE_HOURS * self.HOURLY_RATE) + (
+                self.OVERTIME_HOURS_LIMIT * self.HOURLY_RATE *
+                self.OVERTIME_RATE_1) + (
+                    (overtime_hours - self.OVERTIME_HOURS_LIMIT) *
+                    self.HOURLY_RATE * self.OVERTIME_RATE_2)
 
     @staticmethod
     def _calculate_shift_duration(start_time: str, end_time: str) -> timedelta:
+        """Calculate and return the duration of a shift."""
         start = datetime.strptime(start_time, "%H:%M")
         end = datetime.strptime(end_time, "%H:%M")
         return end - start
 
-    def calculate_total_working_hours_and_wage(self) -> (float, float):
+    def calculate_total_working_hours_and_wage(self) -> Tuple[float, float]:
+        """Calculate and return the total working hours and wage."""
         total_wage = 0
         total_working_time = timedelta(0)
 
         for date, shift in self.shifts.items():
-            if shift == "X":
+            if shift == ShiftType.REST.value:
                 continue
 
             duration = self._calculate_shift_duration(
@@ -92,7 +75,7 @@ class ShiftCalculator:
             if date in self.holidays:
                 print(f"Date: {date} is a holiday")
                 total_wage += duration.total_seconds(
-                ) / 3600 * self.hourly_rate * 2
+                ) / 3600 * self.HOURLY_RATE * self.HOLIDAY_RATE
             else:
                 total_wage += self.calculate_overtime_pay(duration)
 
@@ -105,11 +88,13 @@ class ShiftCalculator:
     def __str__(self) -> str:
         total_working_hours, total_salary = self.calculate_total_working_hours_and_wage(
         )
-        return f"Total working hours: {total_working_hours}\nTotal salary: {total_salary}"
+        return f"Total duty hours: {total_working_hours}\nTotal salary: {total_salary}"
 
 
 if __name__ == "__main__":
-    calculator = ShiftCalculator(Path("./data/shift_type.json"),
-                                 Path("./data/feb_shift.json"),
-                                 Path("./data/holiday.json"), 190)
+    shift_type_file = Path("./data/shift_type.json")
+    shift_file = Path("./data/jan_shift.json")
+    holiday_file = Path("./data/holiday.json")
+
+    calculator = ShiftCalculator(shift_type_file, shift_file, holiday_file)
     print(calculator)
