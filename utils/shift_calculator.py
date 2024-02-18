500cae9cbd8b565c486cea8eba0ccc7e5bc068fa
jimmmm-mm <m11001107@ntust.edu.tw>
2024-02-18 22:20:40 +0800

    chore: rename variables, remove shit

--- utils/shift_calculator.py
+++ utils/shift_calculator.py
@@ -6,11 +6,11 @@ from typing import Any, Dict, Tuple
 from enum import Enum
 
 
-class ShiftType(Enum):
+class Shift(Enum):
     REST = "X"
 
 
-class WageInfo(Enum):
+class Wage(Enum):
     BASE_HOURS = 8
     OVERTIME_HOURS_LIMIT = 2
     OVERTIME_RATE_1 = 1.33
@@ -33,60 +33,58 @@ class ShiftCalculator:
             datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m-%d")
             for date in self.holidays
         ]
-
-        # Assigning WageInfo values to instance variables
         self._set_wage_info()
 
     def _set_wage_info(self):
-        """Set the wage information from the WageInfo Enum."""
-        self.base_hours = WageInfo.BASE_HOURS.value
-        self.overtime_hours_limit = WageInfo.OVERTIME_HOURS_LIMIT.value
-        self.overtime_rate_1 = WageInfo.OVERTIME_RATE_1.value
-        self.overtime_rate_2 = WageInfo.OVERTIME_RATE_2.value
-        self.holiday_rate = WageInfo.HOLIDAY_RATE.value
-        self.hourly_rate = WageInfo.HOURLY_RATE.value
+        self.base_hours = Wage.BASE_HOURS.value
+        self.overtime_hours_limit = Wage.OVERTIME_HOURS_LIMIT.value
+        self.overtime_rate_1 = Wage.OVERTIME_RATE_1.value
+        self.overtime_rate_2 = Wage.OVERTIME_RATE_2.value
+        self.holiday_rate = Wage.HOLIDAY_RATE.value
+        self.hourly_rate = Wage.HOURLY_RATE.value
 
     @staticmethod
     def _load_json_file(file_path: Path) -> Dict[str, Any]:
-        """Load and return data from a JSON file."""
         return json.loads(file_path.read_text())
 
-    def calculate_overtime_pay(self, duty_hours: timedelta) -> float:
-        """Calculate the overtime pay based on the duty hours."""
+    def _calculate_holiday_pay(self, duty_hours: timedelta) -> float:
         duty_hours = duty_hours.total_seconds() / 3600
-        resting_hours = np.ceil(duty_hours / 4) * 0.5
-        working_hours = np.floor([duty_hours - resting_hours])
+        return duty_hours * self.hourly_rate * self.holiday_rate
+
+    def _calculate_overtime_pay(self, duty_hours: timedelta) -> float:
+        duty_hours = duty_hours.total_seconds() / 3600
+        resting_hours = np.round(duty_hours / 4, 0) * 0.5
+        working_hours = duty_hours - resting_hours
         overtime_hours = np.floor(np.max([0, duty_hours - self.base_hours]))
 
         if duty_hours <= self.base_hours:
             return working_hours * self.hourly_rate
         elif overtime_hours <= self.overtime_hours_limit:
-            return (self.base_hours * self.hourly_rate) + (
-                overtime_hours * self.hourly_rate * self.overtime_rate_1)
-        else:  # overtime_hours > 2
-            return (self.base_hours * self.hourly_rate) + (
-                self.overtime_hours_limit * self.hourly_rate *
-                self.overtime_rate_1) + (
-                    (overtime_hours - self.overtime_hours_limit) *
-                    self.hourly_rate * self.overtime_rate_2)
+            base_pay = self.base_hours * self.hourly_rate
+            overtime_pay = overtime_hours * self.hourly_rate * self.overtime_rate_1
+            return base_pay + overtime_pay
+        else:
+            base_pay = self.base_hours * self.hourly_rate
+            overtime_pay_1 = self.overtime_hours_limit * self.hourly_rate * self.overtime_rate_1
+            overtime_pay_2 = (overtime_hours - self.overtime_hours_limit
+                              ) * self.hourly_rate * self.overtime_rate_2
+            return base_pay + overtime_pay_1 + overtime_pay_2
 
     @staticmethod
     def _calculate_shift_duration(start_time: str, end_time: str) -> timedelta:
-        """Calculate and return the duration of a shift."""
         start = datetime.strptime(start_time, "%H:%M")
         end = datetime.strptime(end_time, "%H:%M")
         return end - start
 
     def calculate_total_working_hours_and_wage(self) -> Tuple[float, float]:
-        """Calculate and return the total working hours and wage."""
         total_wage = 0
         total_working_time = timedelta(0)
 
         for _date, shift in self.shifts.items():
-            date = datetime.strptime(_date, "%Y-%m-%d").strftime("%Y-%m-%d")
-            if shift == ShiftType.REST.value:
+            if shift == Shift.REST.value:
                 continue
 
+            date = datetime.strptime(_date, "%Y-%m-%d").strftime("%Y-%m-%d")
             duration = self._calculate_shift_duration(
                 self.shift_type[shift]["start_time"],
                 self.shift_type[shift]["end_time"])
@@ -94,10 +92,9 @@ class ShiftCalculator:
 
             if date in self.holidays:
                 print(f"Date: {date} is a holiday, applying holiday rate.")
-                total_wage += duration.total_seconds(
-                ) / 3600 * self.hourly_rate * self.holiday_rate
+                total_wage += self._calculate_holiday_pay(duration)
             else:
-                total_wage += self.calculate_overtime_pay(duration)
+                total_wage += self._calculate_overtime_pay(duration)
 
         total_working_hours = round(total_working_time.total_seconds() / 3600,
                                     2)
@@ -113,7 +110,7 @@ class ShiftCalculator:
 
 if __name__ == "__main__":
     shift_type_file = Path("./data/shift_type.json")
-    shift_file = Path("./data/feb_shift.json")
+    shift_file = Path("./data/jan_shift.json")
     holiday_file = Path("./data/holiday.json")
     calculator = ShiftCalculator(shift_type_file, shift_file, holiday_file)
     print(calculator)
