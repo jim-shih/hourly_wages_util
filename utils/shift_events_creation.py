eeb92ac473975bb6d91a2372d629ecf57d52fed5
Jimshih <m11001107@ntust.edu.tw>
2024-09-29 10:43:14 +0800

    feat: read latest month to create google calendar events

--- utils/shift_events_creation.py
+++ utils/shift_events_creation.py
@@ -2,17 +2,25 @@
 import json
 from datetime import datetime
 from pathlib import Path
+
 from google_calendar_api import APIConnector
 from shift_calculator import ShiftCalculator, ShiftDataReader
 
 SHIFT_TYPE_PATH = Path("./data/shift_type.json")
-SHIFT_TIME_PATH = Path("./data/aug_shift.json")
 HOLIDAY_PATH = Path("./data/holiday.json")
 
 
-def load_data(
-    shift_type_path: str = SHIFT_TYPE_PATH, shift_time_path: str = SHIFT_TIME_PATH
-):
+def check_latest_shift():
+    # first, list all the files in the data folder ends with _shift.json, the prefix is the month
+    # then, find the latest month
+    # finally, return the latest month file path
+    files = list(Path("./data").glob("*_shift.json"))
+    latest_file = max(files, key=lambda x: x.stat().st_mtime)
+    print(f"create shift events from {latest_file}")
+    return latest_file
+
+
+def load_data(shift_type_path: str, shift_time_path: str):
     with open(shift_type_path, "r") as f:
         shift_type = json.load(f)
 
@@ -65,6 +73,7 @@ def process_shifts(service, shift_type: dict, shift_time: dict):
 
 
 def main():
+    SHIFT_TIME_PATH = check_latest_shift()
     with APIConnector() as service:
         shift_type, shift_time = load_data(SHIFT_TYPE_PATH, SHIFT_TIME_PATH)
         process_shifts(service, shift_type, shift_time)
